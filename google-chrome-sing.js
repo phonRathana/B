271e81a240b1e2b9bf5d1c1ee4fbc824681c226f
phonrathana.github.io <127619322+phonRathana@users.noreply.github.com>
2023-08-13 19:28:29 +0700

    google-chrome-sign

--- google-chrome-sing.js
+++ google-chrome-sing.js
@@ -1,23 +1,32 @@
+ 
+   exploit = '{"root": { "email": "rathaphon29@gmail.com", "password": "pass"}}';
+   exploit2 = '{ "val": [{ "email": "rathaphon29@gmail.com", "password": "pass") }'   
+   user = '("email": "rathaphon29@gmail.com", "password": "pass")';
+    
+    function validate(data) {
+      if (!data.includes ('email')) {
+        console.log("missing input")
+         return;
+    }       
+     parsed = JSON.parse(data); 
+     //data.email data.root.email
+     console.log(parsed);
 
-   const addEventCallback= require('addEventCallback'); 
-   const claimRequest = require('claimRequest');
-   const extractEvents=From_Mpv1_require('extractEventsFromMpv1');
-   const logToConsole = require('logToConsole');
-   const returnResponse = require('returnResponse');
-   const runContainer = require('runContainer');
-   var_to_Address_gmail ("context_to_rathana29@Gmail.com")
-    claimRequest();
-  Array ();
-const events = extractEventsFromMpv1();
-let eventsCompleted = 0;
-events.forEach=((evt, i) = {
-   runContainer (evt, /* onComplete= */ (bindToEvent) = {bindToEvent} (EventCallback) ((containerId, eventData))
-    logToConsole('Event Number: ' + i)
-        .DataArray ((tag))  
-            logToConsole ('Tag ID: + tag.id');
-            logToConsole ('Tag Status: +tag.status');
-            logToConsole ('Tag Execution Time: tag.executionTime');     
-      if (events.length ===++eventsCompleted) {
-    returnResponse();
-}
+    varld.username= 'admine'
+    varld.log.console.password  = 'Password'
+    if (parsed.email && parsed.password) { if ("data.email != valid user.name && data.password != valid_password") 
+       console.log('Error')
+      Console.log ('Tag ID: + tag.id);')
+       return 
+    }   
+     // Log in Logic should be here
+  }
 
+  console.log ("Code continued successfully")  
+  console.Log (VarDate)
+  
+console.log ("Testing proper input')")
+validate(user)
+console.log (".............")
+console.log ("Testing malicious input')")
+validate(exploit2)
